4d9c9f95dbd054ce5308d8f29c76b692a29f61b8
harshitChauhan001 <108900696+harshitChauhan001@users.noreply.github.com>
2024-05-24 22:53:33 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://stackoverflow-server-1hzw.onrender.com/' })
+const API = axios.create({ baseURL: 'https://stackoverflow-server-8syy.onrender.com/' })
 
 
 
